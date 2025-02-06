222c8e57453fdb907d5c36b737cdf0e035fe6455
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-06 14:41:32 -0800

    Implement clock function

---
+++ js/clock.js
@@ -0,0 +1,23 @@
+function tictok() {
+  const clock = document.querySelector('#clock');
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+
+  clock.innerHTML = `
+    <div class="clock-time">
+      <strong>${hours}</strong><span> : </span><strong>${minutes}</strong><span> : </span><strong>${seconds}</strong>
+    </div>
+    <div class="clock-date">
+      <span>${year}</span>-<span>${month}</span>-<span>${day}</span>
+    </div>
+  `;
+}
+
+tictok();
+setInterval(tictok, 1000);
