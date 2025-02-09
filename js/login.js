ef92690c1a270928a90eacbb5338b4bed0fbc0c3
Chole Seoyeong Ro <nostopzero@gmail.com>
2025-02-09 11:21:11 -0800

    Implement Login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const form = document.querySelector('#login-form');
+const form = document.querySelector('#login');
 const loginName = form.querySelector('#login-name');
 const greeting = document.querySelector("#greeting");
 
@@ -20,6 +20,7 @@ function paintingGreeting(username) {
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
+console.log(savedUsername)
 if(savedUsername === null) {
   form.classList.remove(HIDDEN_CLASSNAME)
   form.addEventListener('submit', handleLogin);
